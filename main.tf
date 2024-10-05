d224faec33340de380fe01cfc3a58ac84778de1d
JeffDegoma <jhdegoma@hotmail.com>
2024-10-06 04:51:56 +0800

    reconfigure subnet names

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ module "vpc" {
   public_subnet_names = ["management-a", "management-b"]
   azs             = ["us-east-1a", "us-east-1b"] //more azs
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  # private_subnets = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24", "10.0.5.0/24"]
   database_subnets = ["10.0.6.0/24", "10.0.7.0/24", "10.0.8.0/24"]
   create_igw      = true
   enable_dns_hostnames = true
