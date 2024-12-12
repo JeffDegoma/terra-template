bb60590de12968c287f470400c61c6bab325da71
JeffDegoma <jhdegoma@hotmail.com>
2024-12-12 08:35:02 +0800

    comment out so only main.tf file gets read

--- remote.tf
+++ remote.tf
@@ -1,15 +1,15 @@
-provider "aws" {
-    version =  ">= 4.55"
-    region  = var.region
-}
+# provider "aws" {
+#     version =  ">= 4.55"
+#     region  = var.region
+# }
 
-terraform {  
-    backend "s3" {
-        bucket  = "terraform-backend-pakil-state"
-        encrypt = true
-        key     = "terraform.tfstate"    
-        region  = "us-east-1"
-    }
-}
+# terraform {  
+#     backend "s3" {
+#         bucket  = "terraform-backend-pakil-state"
+#         encrypt = true
+#         key     = "terraform.tfstate"    
+#         region  = "us-east-1"
+#     }
+# }
 
 
