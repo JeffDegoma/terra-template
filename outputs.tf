5f75443e276ae65b33b685d4fc399d1eb69a87b9
JeffDegoma <jhdegoma@hotmail.com>
2024-12-15 15:52:36 +0800

    test remote state

--- outputs.tf
+++ outputs.tf
@@ -13,6 +13,11 @@ output "ami" {
 }
 
 
+output "vpc_name"{
+  description = "load balancer dns"
+  value = module.vpc.name
+}
+
 output "ec2_complete_public_ip" {
   description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
   value       = module.ec2_instance.public_ip
