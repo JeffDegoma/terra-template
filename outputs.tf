4c0e010eb48eef1f7730409a36aa4d9f21b5081c
JeffDegoma <jhdegoma@hotmail.com>
2024-12-16 10:47:06 +0800

    removed outputs

--- outputs.tf
+++ outputs.tf
@@ -3,14 +3,6 @@ output "RDS_ENDPOINT" {
   value       = module.db.db_instance_endpoint
 }
 
-output "alb" {
-  description = "load balancer dns"
-  value = module.alb.lb_dns_name
-}
-output "ami" {
-  description = "ami name"
-  value = data.aws_ami.packer-custom-ami.id
-}
 
 
 output "vpc_name"{
@@ -18,7 +10,3 @@ output "vpc_name"{
   value = module.vpc.name
 }
 
-output "ec2_complete_public_ip" {
-  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as this field will change after the EIP is attached"
-  value       = module.ec2_instance.public_ip
-}
